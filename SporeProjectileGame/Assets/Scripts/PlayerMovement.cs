e41280c6c6c02ac4d3c2d4ced7923a1c540dd30e
MitchellCaldwell29 <mitchell.caldwell18@hotmail.com>
2023-05-27 22:17:25 +1000

    Colour change to ground
    Changed colour to light blue

---
+++ SporeProjectileGame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 1f;
+    private void Update()
+    {
+        float xAxis = Input.GetAxis("Horizontal");
+        float zAxis = Input.GetAxis("Vertical"); 
+
+        Vector3 moveDirecton = new Vector3(xAxis, 0.0f, zAxis);
+
+        transform.position += moveDirecton * speed;
+    }
+}
