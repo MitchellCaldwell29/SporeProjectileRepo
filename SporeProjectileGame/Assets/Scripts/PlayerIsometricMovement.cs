e109487ff757fa62fa5dc83a93cd75e737ff2273
MitchellCaldwell29 <131213839+MitchellCaldwell29@users.noreply.github.com>
2023-06-07 17:00:19 +1000

    Movement
    Character movement now does a full rotation and the angle of the x,y,z axis now faces true up, down, left and right

--- SporeProjectileGame/Assets/Scripts/PlayerIsometricMovement.cs
+++ SporeProjectileGame/Assets/Scripts/PlayerIsometricMovement.cs
@@ -6,7 +6,8 @@ using UnityEngine;
 public class PlayerIsometricMovement : MonoBehaviour
 {
     [SerializeField] private Rigidbody playerRb;
-    [SerializeField] private float playerSpeed = 5; 
+    [SerializeField] private float playerSpeed = 5;
+    [SerializeField] private float turnSpeed = 360; 
     private Vector3 playerInput;
 
     void Update()
@@ -27,14 +28,22 @@ public class PlayerIsometricMovement : MonoBehaviour
 
     void Look()
     {
-        var direction = (transform.position + playerInput) - transform.position; 
-        var lookRotation = Quaternion.LookRotation(direction,Vector3.up);
-        
-        transform.rotation = lookRotation; 
+        if (playerInput != Vector3.zero)
+        {
+            var matrix = Matrix4x4.Rotate(Quaternion.Euler(0,45,0)); 
+
+            var alteredInput = matrix.MultiplyPoint3x4(playerInput);
+
+            var direction = (transform.position + alteredInput) - transform.position;
+            var lookRotation = Quaternion.LookRotation(direction, Vector3.up);
+
+            transform.rotation = Quaternion.RotateTowards(transform.rotation,lookRotation,turnSpeed * Time.deltaTime);
+        }
+
     }
 
     void Move()
     {
-        playerRb.MovePosition(transform.position + transform.forward * playerSpeed * Time.deltaTime); 
+        playerRb.MovePosition(transform.position + (transform.forward * playerInput.magnitude) * playerSpeed * Time.deltaTime); 
     }
 }
