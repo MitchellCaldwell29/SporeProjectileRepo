6be54cc261152e7e0e91859ecda5b962fcf840ec
MitchellCaldwell29 <mitchell.caldwell18@hotmail.com>
2023-06-14 21:27:24 +1000

    Feature (raycast) #2
    Instantiates a cube on the surface where the raycast is hitting.

--- SporeProjectileGame/Assets/Scripts/RayCastAim.cs
+++ SporeProjectileGame/Assets/Scripts/RayCastAim.cs
@@ -5,12 +5,14 @@ using UnityEngine.UI;
 
 public class RayCastAim : MonoBehaviour
 {
-    Camera cam;
+    private Camera cam = null;
     public LayerMask mask;
 
     Vector3 aimCubePosition;
     Vector3 AimCubePosition;
-    Vector3 RayTest; 
+    Vector3 RayTest;
+
+    public GameObject rayCastGroundTest = null; 
 
     // Start is called before the first frame update
     void Start()
@@ -21,7 +23,11 @@ public class RayCastAim : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Draw Ray
+        DrawRay(); 
+    }
+
+    void DrawRay()
+    {
         Vector3 mousePos = Input.mousePosition;
         mousePos.z = 10f;
         mousePos = cam.ScreenToWorldPoint(mousePos);
@@ -29,25 +35,25 @@ public class RayCastAim : MonoBehaviour
         Ray ray = cam.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
 
-        if (Physics.Raycast(ray, out hit))
-        {
-            aimCubePosition = hit.point;
-            AimCubePosition = hit.normal;
-            RayTest = hit.point;
-            Debug.Log(RayTest);
-        }
-
         if (Input.GetMouseButtonDown(0))
         {
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Instantiate(rayCastGroundTest, new Vector3(hit.point.x, hit.point.y, hit.point.z), Quaternion.identity);
+
+                aimCubePosition = hit.point;
+                AimCubePosition = hit.normal;
+                RayTest = hit.point;
+                Debug.Log(RayTest);
+            }
+
             if (Physics.Raycast(ray, out hit, 500, mask))
             {
                 Debug.Log(hit.transform.name);
                 hit.transform.GetComponent<Renderer>().material.color = Color.red;
             }
-
         }
-
-
     }
 
     private void OnDrawGizmos()
