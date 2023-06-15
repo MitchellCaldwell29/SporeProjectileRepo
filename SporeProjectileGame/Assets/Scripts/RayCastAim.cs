a89befac8e48ba9733d9634cb06eb48ab1bd984a
MitchellCaldwell29 <131213839+MitchellCaldwell29@users.noreply.github.com>
2023-06-15 11:53:47 +1000

    Feature/Fix (Post production/ ray cast) #1
    Post production added for showing objects hidden from camera (player). Ray cast now fulfills all functions for turret rotation by raycasting to a layer, updating the position of a turret raycast and making the turret rotate towards that position.

--- SporeProjectileGame/Assets/Scripts/RayCastAim.cs
+++ SporeProjectileGame/Assets/Scripts/RayCastAim.cs
@@ -5,14 +5,11 @@ using UnityEngine.UI;
 
 public class RayCastAim : MonoBehaviour
 {
-    private Camera cam = null;
+    private Camera cam;
     public LayerMask mask;
 
-    Vector3 aimCubePosition;
-    Vector3 AimCubePosition;
-    Vector3 RayTest;
-
-    public GameObject rayCastGroundTest = null; 
+    public GameObject turretRayPosition; 
+    public float rotationSpeed;
 
     // Start is called before the first frame update
     void Start()
@@ -35,34 +32,19 @@ public class RayCastAim : MonoBehaviour
         Ray ray = cam.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
 
+        if (Physics.Raycast(ray, out hit, 500, mask))
+        {
+            turretRayPosition.transform.LookAt(new Vector3(hit.point.x, 1.35f, hit.point.z)); //Testing to see if camera will rotate. 
+            transform.rotation = Quaternion.Slerp(transform.rotation, turretRayPosition.transform.rotation, Time.fixedDeltaTime * rotationSpeed);
+        }
+
         if (Input.GetMouseButtonDown(0))
         {
-
-            if (Physics.Raycast(ray, out hit, 500, mask))
-            {
-                transform.LookAt(new Vector3(hit.point.x, 1.35f, hit.point.z)); //Testing to see if camera will rotate. 
-
-                
-                Instantiate(rayCastGroundTest, new Vector3(hit.point.x, hit.point.y, hit.point.z), Quaternion.identity);
-
-                aimCubePosition = hit.point;
-                AimCubePosition = hit.normal;
-                RayTest = hit.point;
-                Debug.Log(RayTest);
-            }
-
             if (Physics.Raycast(ray, out hit, 500, mask))
             {
                 Debug.Log(hit.transform.name);
                 hit.transform.GetComponent<Renderer>().material.color = Color.red;
             }
         }
-    }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawSphere(aimCubePosition, 0.5f);
-
     }
 }
