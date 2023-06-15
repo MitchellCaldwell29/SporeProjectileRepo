51a1ad2415291040607466c2e493e320249f4a0a
MitchellCaldwell29 <131213839+MitchellCaldwell29@users.noreply.github.com>
2023-06-15 12:36:40 +1000

    Revert "Revert "Feature/Fix (Post production/ ray cast) #1""
    This reverts commit 3829d1475364bef384c724dfc6dc3770de6935a0.

--- SporeProjectileGame/Assets/Scripts/TurretRotation.cs
+++ SporeProjectileGame/Assets/Scripts/TurretRotation.cs
@@ -9,15 +9,13 @@ public class TurretRotation : MonoBehaviour
 
     private Quaternion lookRotation;
     private Vector3 direction; 
+
     // Update is called once per frame
     void Update()
     {
-
-            direction = (mousePosition.position - transform.position).normalized;
-            lookRotation = Quaternion.LookRotation(direction);
-            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * rotationSpeed);
-
-
+        direction = (mousePosition.position - transform.position); 
+        lookRotation = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * rotationSpeed);
         //Vector3 lookAtRotation = Quaternion.LookRotation(mousePosition.position - transform.position).eulerAngles;
         //transform.rotation = Quaternion.Euler(Vector3.Scale(lookAtRotation, new Vector3 (0,1,0)));
 
